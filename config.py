9e328318a647e0a8c76c9a2474f65f4f022db5f8
Minimaxgithub <lakshman9440823127@gmail.com>
2025-01-24 08:12:22 +0530

    commits

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+API_KEY = "b5a4198a673b638f81aa296e83966590"
+
+# Base URLs
+BASE_URL = "https://api.themoviedb.org/3"
+IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+PLACEHOLDER_IMAGE = "https://via.placeholder.com/500x750?text=No+Poster+Available"
+
+# Proxy settings (update if needed)
+PROXY = {
+    'http': None,  # Set your HTTP proxy, if required
+    'https': None  # Set your HTTPS proxy, if required
+}
